75ff86e7237af023649d75a27260313a5dded9fc
MahdiRezai <engineer.m.rezai@gmail.com>
2026-08-10 10:41:29 +0330

    registered `Contact` model in admin panel with `ContactAdmin` customization

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from website.models import Contact
 
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name','email','subject','created_date','updated_date']
+    list_filter = ['email']
+    search_fields = ['name','email','subject']
+
+
