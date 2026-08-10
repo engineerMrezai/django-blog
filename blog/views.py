f2d99aeeb536661e049ac5147a9f5e7a4bfd7c2e
MahdiRezai <engineer.m.rezai@gmail.com>
2026-08-10 17:17:28 +0330

    integrated dynamic post rendering in blog views and templates

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
+from blog.models import Post
+
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1,published_date__lt=timezone.now())
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html',context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request,id):
+    post = get_object_or_404(Post,id=id,status=1, published_date__lt=timezone.now())
+    post.counted_view += 1
+    post.save()
+    content = {'post':post}
+    return render(request, 'blog/blog-single.html',content)
