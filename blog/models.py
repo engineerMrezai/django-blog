66483529719721d970bff73879c2f7a7efd1a5eb
MahdiRezai <engineer.m.rezai@gmail.com>
2026-08-10 10:19:57 +0330

    added initial migrations and models for blog and website apps

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField()
+    content = models.TextField()
+    # image = models.ImageField()
+    # category = models.ManyToManyField('Category', related_name='blog_posts')
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='blog_posts')
+    # tags = models.ManyToManyField('Tag', related_name='blog_posts')
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.title
+
+# class Category(models.Model):
+#     name = models.CharField()
+#     created_date = models.DateTimeField()
+#     updated_date = models.DateTimeField()
+#
+# class Tag(models.Model):
+#     name = models.CharField()
+#     created_date = models.DateTimeField()
+#     updated_date = models.DateTimeField()
+#
+
+
--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField()
+    email = models.EmailField()
+    subject = models.CharField()
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
